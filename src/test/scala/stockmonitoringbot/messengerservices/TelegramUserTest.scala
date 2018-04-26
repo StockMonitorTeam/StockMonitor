b080ab3a4dac77dfec0aedf893a764da37367c3e
N0str <ntpcp@yandex.ru>
2018-04-26 19:34:53 +0300

    Fix tests

--- src/test/scala/stockmonitoringbot/messengerservices/TelegramUserTest.scala
+++ src/test/scala/stockmonitoringbot/messengerservices/TelegramUserTest.scala
@@ -7,6 +7,7 @@ import org.scalatest.{FlatSpec, Matchers}
 import stockmonitoringbot.ExecutionContextComponent
 import stockmonitoringbot.datastorage.InMemoryDataStorage
 import stockmonitoringbot.messengerservices.UserActor.IncomingMessage
+import stockmonitoringbot.messengerservices.markups.{Buttons, GeneralMarkups, GeneralTexts}
 
 import scala.concurrent.ExecutionContext
 
@@ -35,16 +36,17 @@ class TelegramUserTest extends FlatSpec with Matchers {
   "UserActor" should "send hello message on creating" in {
     system.actorOf(UserActor.props(0, messageSenderMock, dataBaseMock))
     Thread.sleep(100)
-    outgoingMessages.head shouldBe SendMessage(ChatId(0), "Hello, choose action:", replyMarkup = UserActor.startMenuMarkup)
+    outgoingMessages.head shouldBe SendMessage(ChatId(0), GeneralTexts.INTRO_MESSAGE, replyMarkup = GeneralMarkups.startMenuMarkup)
   }
 
   "UserActor" should "return stock price" in {
     dataBaseMock.addStock(stock, 23)
     val user = system.actorOf(UserActor.props(0, messageSenderMock, dataBaseMock))
-    user ! IncomingMessage(UserActor.getStockPrice)
+    user ! IncomingMessage(Buttons.stock)
     user ! IncomingMessage("MSFT")
     Thread.sleep(100)
-    outgoingMessages.tail.head shouldBe SendMessage(ChatId(0), s"$stock current price is 23.0")
+
+    outgoingMessages.head shouldBe SendMessage(ChatId(0), GeneralTexts.printStockPrice("MSFT", 23.0))
   }
 
 
