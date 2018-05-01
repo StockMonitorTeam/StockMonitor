5e474af8f511b831dd2d4543cc0f035a52e2bbcf
AmirMuratov <amir1muratov@gmail.com>
2018-05-01 12:58:31 +0300

    fix: InMemoryUserDataStorage doesn't use execution context now

--- src/main/scala/stockmonitoringbot/datastorage/InMemoryUserDataStorageComponentImpl.scala
+++ src/main/scala/stockmonitoringbot/datastorage/InMemoryUserDataStorageComponentImpl.scala
@@ -12,7 +12,6 @@ import scala.concurrent.Future
   */
 
 trait InMemoryUserDataStorageComponentImpl extends UserDataStorageComponent {
-  this: ExecutionContextComponent =>
 
   override val userDataStorage: UserDataStorage = new InMemoryUserDataStorage
 
