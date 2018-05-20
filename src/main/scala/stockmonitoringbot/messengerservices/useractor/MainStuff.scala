c25d558018849e2941c29e49700d5e218f8b2a24
AmirMuratov <amir1muratov@gmail.com>
2018-05-20 21:58:47 +0300

    fix

--- src/main/scala/stockmonitoringbot/messengerservices/useractor/MainStuff.scala
+++ src/main/scala/stockmonitoringbot/messengerservices/useractor/MainStuff.scala
@@ -14,7 +14,7 @@ import stockmonitoringbot.messengerservices.markups.{Buttons, GeneralMarkups, Ge
 import stockmonitoringbot.messengerservices.useractor.UserActor.{CallbackTypes, IncomingCallback, IncomingMessage, SetBehavior}
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 /**
   * Created by amir.
@@ -113,14 +113,17 @@ trait MainStuff {
   }
 
   def setNotification(userId: Long, assetType: AssetType, time: String, user: User): Future[Unit] = {
-    val task = for {_ <- clearNotification(userId, assetType)
-                    localTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("H:mm"))
-                    utcTime = getTimeInUTC(localTime, user.timeZone)
-                    notification = assetType match {
-                      case PortfolioAsset(name) => PortfolioDailyNotification(0, userId, name, utcTime)
-                      case StockAsset(name) => StockDailyNotification(0, userId, name, utcTime)
-                      case ExchangeRateAsset(from, to) => ExchangeRateDailyNotification(0, userId, (from, to), utcTime)
-                    }
+    val notificationTry = Try {
+      val localTime: LocalTime = LocalTime.parse(time, DateTimeFormatter.ofPattern("H:mm"))
+      val utcTime = getTimeInUTC(localTime, user.timeZone)
+      assetType match {
+        case PortfolioAsset(name) => PortfolioDailyNotification(0, userId, name, utcTime)
+        case StockAsset(name) => StockDailyNotification(0, userId, name, utcTime)
+        case ExchangeRateAsset(from, to) => ExchangeRateDailyNotification(0, userId, (from, to), utcTime)
+      }
+    }
+    val task = for {notification <- Future.fromTry(notificationTry)
+                    _ <- clearNotification(userId, assetType)
                     _ <- userActorService.addDailyNotification(notification)
     } yield {
       sendMessageToUser(GeneralTexts.DAILY_NOTIFICATION_SET(time))
